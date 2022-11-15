10415caf0a10a4a24b851d8f5d5a854239b08939
Ryan Vales <59878858+ImArvy@users.noreply.github.com>
2022-11-15 11:11:08 -0600

    Update gallery.js

--- Class Project/scripts/gallery.js
+++ Class Project/scripts/gallery.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
     //for (let i = 0; i < 3334; i++) {
     for (let i = 0; i < 50; i++) {
         $("#right").html("");
-        $.getJSON("data/" + i + ".json", (data) => {
+        $.getJSON("../data/" + i + ".json", (data) => {
             $("#right").append("<a href = details.html id = " + i + "><img src = https://s7nspfp.mypinata.cloud/ipfs/" + data.image + "></a>");
         });
     }
