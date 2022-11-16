67e07e47cf25d8b97d5e564483efe57943651984
Ryan Vales <ryanvales@rocketmail.com>
2022-11-15 21:38:10 -0600

    changes

--- Class Project/scripts/details.js
+++ Class Project/scripts/details.js
@@ -1,4 +1,13 @@
 $(document).ready(() => {
+    let id = localStorage['jsonNumber'];
+    localStorage.removeItem('jsonNumber');
+    $("#details").html("");
+    $.getJSON("../data/" + id + ".json", (data) => {
+        let attributes = data.attributes;
+        for (let j = 0; j < attributes.length; j++) {
+            $("#details").append("<li><h2>" + attributes[j].trait_type + "</h2><h3>" + attributes[j].value + "</h3></li>");
+        }
+    });
     /*
     for (let i = 0; i < 50; i++) {
         $("#details").html("");
--- Class Project/scripts/gallery.js
+++ Class Project/scripts/gallery.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
     for (let i = 0; i < 50; i++) {
         $("#right").html("");
         $.getJSON("../data/" + i + ".json", (data) => {
-            $("#right").append("<a href = details.html id = " + i + "><img src = https://s7nspfp.mypinata.cloud/ipfs/" + data.image + "></a>");
+            $("#right").append("<a href = details.html><img src = https://s7nspfp.mypinata.cloud/ipfs/" + data.image + " id = " + i + "></a>");
         });
     }
 
@@ -12,12 +12,7 @@ $(document).ready(() => {
         let id = $(this).attr("id");
         $(this).click((evt) => {
             evt.preventDefault();
-            $.getJSON("../data/" + id + ".json", (data) => {
-                let attributes = data.attributes;
-                for (let i = 0; i < attributes.length; i++) {
-                    $("#details").append("<li><h2>" + attributes[i].trait_type + "</h2><h3>" + attributes[i].value + "</h3></li>");
-                }
-            });
+            localStorage.setItem( 'jsonNumber', id );
         });
     });
 });
