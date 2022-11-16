07dd5b90a00aaf5be503308fd85d1fbff509f991
Ryan Vales <ryanvales@rocketmail.com>
2022-11-15 23:27:33 -0600

    Update gallery.js

--- Class Project/scripts/gallery.js
+++ Class Project/scripts/gallery.js
@@ -1,16 +1,13 @@
-//for (let i = 0; i < 3334; i++) {
-for (let i = 0; i < 50; i++) {
-    $("#right").html("");
-    $.getJSON("../data/" + i + ".json", (data) => {
-        $("#right").append("<a href = details.html><img src = https://s7nspfp.mypinata.cloud/ipfs/" + data.image + " id = " + i + " onclick = ></a>");
-    });
-}
-
-let images = $("#right").find("img");
-console.log(images);
-
 $(document).ready(() => {
-    let images = $("#right").find("img");
+    //for (let i = 0; i < 3334; i++) {
+    for (let i = 0; i < 50; i++) {
+        $("#right").html("");
+        $.getJSON("../data/" + i + ".json", (data) => {
+            $("#right").append("<a href = details.html><img src = https://s7nspfp.mypinata.cloud/ipfs/" + data.image + " id = " + i + " onclick = ></a>");
+        });
+    }
+
+    let images = $("div#right").find("img");
     console.log(images);
     $(images).each(() => {
         let id = $(this).attr("id");
