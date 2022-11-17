6cdad3cb2329640f407a4cda5f5a1f00bbe86efa
Ryan Vales <ryanvales@rocketmail.com>
2022-11-17 13:50:24 -0600

    Major changes

--- Class Project/scripts/details.js
+++ Class Project/scripts/details.js
@@ -1,22 +1,41 @@
 $(document).ready(() => {
-    let id = localStorage.getItem("number");
-    localStorage.removeItem("number");
-    $("#details").html("");
+    displayData();
+});
+
+let loadJSON = (id) => {
+    $("#box").html("");
     $.getJSON("../data/" + id + ".json", (data) => {
         let attributes = data.attributes;
         for (let j = 0; j < attributes.length; j++) {
-            $("#details").append("<li><h2>" + attributes[j].trait_type + "</h2><h3>" + attributes[j].value + "</h3></li>");
+            $("#box").append("<div><h2>" + attributes[j].trait_type + "</h2><h3>" + attributes[j].value + "</h3></div>");
         }
     });
-    /*
-    for (let i = 0; i < 50; i++) {
-        $("#details").html("");
-        $.getJSON("../data/" + i + ".json", (data) => {
-            let attributes = data.attributes;
-            for (let j = 0; j < attributes.length; j++) {
-                $("#details").append("<li><h2>" + attributes[j].trait_type + "</h2><h3>" + attributes[j].value + "</h3></li>");
-            }
-        });
+};
+
+let decode = (text) => {
+    text = text.replace(/\+/g, " ");
+    text = text.replace(/%[a-fA-F0-9]{2}/g, 
+        (text) => {
+            return String.fromCharCode("0x" + text.substr(1,2));
+        }
+    );
+    return text;
+};
+    
+let displayData = () => {
+    let query = location.search.replace("?", "");
+    if (query == "") return;
+    
+    let fields = query.split("&");
+    if (fields.length == 0) {
+        document.write("<p>No data retrieved.</p>");
+    } else {
+        let field_parts;
+        for (const i in fields) {
+            field_parts = fields[i].split("=");
+            field_parts[0] = decode(field_parts[0]);
+            field_parts[1] = decode(field_parts[1]);
+            loadJSON(field_parts[1]);
+        }
     }
-    */
-});
+};
--- Class Project/scripts/gallery.js
+++ Class Project/scripts/gallery.js
@@ -1,20 +1,12 @@
 $(document).ready(() => {
-    //for (let i = 0; i < 3334; i++) {
-    for (let i = 0; i < 50; i++) {
+    loadJSON();
+});
+
+let loadJSON = () => {
+    for (let i = 0; i < 100; i++) {
         $("#right").html("");
         $.getJSON("../data/" + i + ".json", (data) => {
-            $("#right").append("<a href = details.html><img src = https://s7nspfp.mypinata.cloud/ipfs/" + data.image + " id = " + i + " onclick = ></a>");
+            $("#right").append("<a href = details.html?id=" + i + "><img src = https://s7nspfp.mypinata.cloud/ipfs/" + data.image + " id = " + i + "></a>");
         });
     }
-
-    let images = $("div#right").find("img");
-    console.log(images);
-    $(images).each(() => {
-        let id = $(this).attr("id");
-        console.log(id);
-        $(this).click((evt) => {
-            evt.preventDefault();
-            localStorage.setItem("number", id);
-        });
-    });
-});
+};
